2bc8734fdd8da20b03fa9f0584863076a7276fc3
mnaofficial <mna.official92@gmail.com>
2022-05-20 20:03:43 +0600

    Pages Create

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 import Navbar from './Components/Navbar';
+import About from './Pages/About';
+import Contact from './Pages/Contact';
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Services from './Pages/Services';
 
 function App() {
   return (
-    <div>
-      <Navbar/>
-    </div>
+    <>
+      <Navbar>
+        <Home />
+        <About />
+        <Services />
+        <Contact />
+        <Login />
+      </Navbar>
+    </>
   );
 }
 
---
+++ src/Pages/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const NotFound = () => {
+    return (
+        <div>
+            <h1 className="text-4xl text-red-700">4 0 4</h1>
+        </div>
+    );
+};
+
+export default NotFound;
